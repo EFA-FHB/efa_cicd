2fac40680fe0d46c6a31bc7b62fa7e131edcfe5b
Robert Markolwitz <robert.markolwitz@nortal.com>
2022-09-01 07:56:46 +0200

    [D603345-494] docu + additional logging + env var fixing

--- .github/actions/import_container_images/import_container_images.sh
+++ .github/actions/import_container_images/import_container_images.sh
@@ -1,19 +1,17 @@
 #!/bin/bash -e
 
-touch new_images.txt
-touch old_images.txt
-
 for type in old new; do {
 
   if [[ "$type" == "new" ]]; then
-    subscr_id=$NEW_ACR_SUBSCRIPTION_ID
-    acr_name=$NEW_ACR_RESOURCE_NAME
+    subscr_id="${DEST_ACR_SUBSCRIPTION_ID}"
+    acr_name="${DEST_ACR_RESOURCE_NAME}"
   else
-    subscr_id=$OLD_ACR_SUBSCRIPTION_ID
-    acr_name=$OLD_ACR_RESOURCE_NAME
+    subscr_id="${SOURCE_ACR_SUBSCRIPTION_ID}"
+    acr_name="${SOURCE_ACR_RESOURCE_NAME}"
   fi
 
   az account set -n $subscr_id
+  echo "Fetching repostories of registry $acr_name..."
 
   repos=$(az acr repository list -n "$acr_name" | jq .[] | tr -d '"')
 
@@ -30,22 +28,29 @@ for type in old new; do {
 } done
 
 while read -r -a image; do {
+  echo "Checking image $image..."
   grep -E "$image" new_images.txt -q
-  [[ $(echo $?) -eq 0 ]] && echo $image >> images_to_sync.txt
+  if [[ $(echo $?) -eq 0 ]]; then
+    echo "Import needed"
+    echo $image >> images_to_sync.txt
+  else
+    echo "Already imported, skipping..."
+  fi
+
 } done << old_images.txt
 
 echo "Found $(wc -l images_to_sync.txt) images to import"
 
-az account set -n "${NEW_ACR_SUBSCRIPTION_ID}"
+az account set -n "${DEST_ACR_SUBSCRIPTION_ID}"
 
 n=0;
 while read -r -a image; do {
-  echo "Importing image $image to ${NEW_ACR_NAME}"
+  echo "Importing image $image to ${DEST_ACR_RESOURCE_NAME}"
   az acr import \
-     --name ${NEW_ACR_NAME} \
-     --source $repo:$tag \
-     --image $repo:$tag \
-     --registry /subscriptions/${OLD_ACR_SUBSCRIPTION_ID}/resourceGroups/${OLD_ACR_RESOURCE_GROUP}/providers/Microsoft.ContainerRegistry/registries/${OLD_ACR_RESOURCE_NAME}
+     --name "${DEST_ACR_RESOURCE_NAME}" \
+     --source "$image" \
+     --image "$image" \
+     --registry "/subscriptions/${SOURCE_ACR_SUBSCRIPTION_ID}/resourceGroups/${SOURCE_ACR_RESOURCE_GROUP}/providers/Microsoft.ContainerRegistry/registries/${SOURCE_ACR_RESOURCE_NAME}"
   n=$(($n + 1));
   echo "Import done for $image"
 } << done images_to_sync.txt
