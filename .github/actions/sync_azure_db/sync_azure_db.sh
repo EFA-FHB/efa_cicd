15dc68b997719dab53167d414753e997ccae4f12
Robert Markolwitz <robert.markolwitz@nortal.com>
2022-09-04 12:41:27 +0200

    [D603345-494] fix app reference

--- .github/actions/sync_azure_db/sync_azure_db.sh
+++ .github/actions/sync_azure_db/sync_azure_db.sh
@@ -11,12 +11,12 @@ echo "got pod ${pod}"
 
 exec="kubectl exec $pod"
 
-for db in bkms-mediator vergabesystem-adapter; do {
+for app in bkms-mediator vergabesystem-adapter; do {
 
-  echo -e "retrieving psql_wrapper for ${db}...\n"
-  psql_cli=$($exec -- sh -c "cat ~/.bashrc | grep bkms-mediator | cut -d "=" -f2 | tr -d ''\'")
+  echo -e "retrieving psql_wrapper for ${app}...\n"
+  psql_cli=$($exec -- sh -c "cat ~/.bashrc | grep -E "${app}" | cut -d "=" -f2 | tr -d ''\'")
   echo "creating dump file..."
-  dump_file="${db}_$(date +%F_%T | sed 's/:/_/g').dump"
+  dump_file="${app}_$(date +%F_%T | sed 's/:/_/g').dump"
   $exec -- sh -c "$psql_cli pg_dump -h ${SOURCE_DB_HOST} -Fc -f ${dump_file} --clean -n public -x"
   echo "Importing dump file..."
   $exec -- sh -c "$psql_cli pg_restore -h ${TARGET_DB_HOST} -c < ${dump_file} || true"
